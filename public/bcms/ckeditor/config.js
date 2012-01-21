ff0cc1d9f7765ba46fe019c8356a1dd3cd2b3d2a
Matthew Savage <matt@amasses.net>
2012-01-21 21:41:01 +1100

    Updated the dimensions of the editor, added Formatting option to menu bar

--- public/bcms/ckeditor/config.js
+++ public/bcms/ckeditor/config.js
@@ -1,18 +1,18 @@
 CKEDITOR.config.toolbar_CMS = [
   ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker','Scayt','-','Undo','Redo','Find','Replace','RemoveFormat','-','NumberedList','BulletedList','Outdent','Indent','HorizontalRule'],
   '/',
-  ['Link','Unlink','Anchor','Image','Table','SpecialChar','-','Bold','Italic','Underline','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-','TextColor','Styles']
+  ['Link','Unlink','Anchor','Image','Table','SpecialChar','-','Bold','Italic','Underline','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-','TextColor','Styles','Formatting']
 ];
 
 CKEDITOR.config.toolbar_CMSForms = [
 	['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','SpellChecker','Scayt','-','Undo','Redo','Find','Replace','RemoveFormat','-','NumberedList','BulletedList','Outdent','Indent','HorizontalRule'],
   '/',
-	['Link','Unlink','Anchor','Image','Table','SpecialChar','Bold','Italic','Underline','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','Styles'],
+	['Link','Unlink','Anchor','Image','Table','SpecialChar','Bold','Italic','Underline','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','TextColor','Styles','Formatting'],
 	'/',
 	['TextField','Select','Checkbox','Radio','Textarea','Button','ImageButton','HiddenField']
 ];
 
-CKEDITOR.config.width = 598
+CKEDITOR.config.width = 730
 CKEDITOR.config.height = 400
 
 CKEDITOR.config.toolbar = 'CMS'
